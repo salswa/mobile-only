c7b08136b916063bd59b03f277dc6234e8ad5bd8
sgautam <swati.gautam@wundermanthompson.com>
2023-10-09 01:46:12 +0530

    script.js modified

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Detect if the user is on a mobile device
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 // Redirect if not on a mobile device
-if (!isMobile) {
-    window.location.href = 'not-desktop-compatible.html';
-}
+// if (!isMobile) {
+//     window.location.href = 'not-desktop-compatible.html';
+// }
 
 (function() {
   
